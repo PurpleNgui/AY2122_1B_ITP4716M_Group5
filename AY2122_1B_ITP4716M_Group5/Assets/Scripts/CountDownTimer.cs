75c1081bc598d76f3e7dbb454281b204325c9d58
PurpleNgui <210453770@stu.vtc.edu.hk>
2022-03-31 09:59:47 +0800

    improve timer

--- AY2122_1B_ITP4716M_Group5/Assets/Scripts/CountDownTimer.cs
+++ AY2122_1B_ITP4716M_Group5/Assets/Scripts/CountDownTimer.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CountDownTimer : MonoBehaviour
 {
@@ -13,27 +14,49 @@ public class CountDownTimer : MonoBehaviour
     private float second = 0;
     public bool isRunning = true;
 
+    [SerializeField]
+    private Text text;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        text = GetComponent<Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        minute = Mathf.FloorToInt(timer / 60);
+        minute = Mathf.Floor(timer / 60);
         if (minute < 0)
         {
             minute = 0;
         }
 
-        second = Mathf.FloorToInt(timer % 60);
+        second = Mathf.Ceil(timer % 60);
         if (second < 0)
         {
             second = 0;
         }
 
+        if (minute < 10 && second < 10)
+        {
+            text.text = "0" + minute.ToString() + " : " + "0" + second.ToString();
+        }
+        else if (minute < 10 && second >= 10)
+        {
+            text.text = "0" + minute.ToString() + " : " + second.ToString();
+        }
+        else if (minute >= 10 && second < 10)
+        {
+            text.text = minute.ToString() + " : " + "0" + second.ToString();
+        }
+        else
+        {
+            text.text = minute.ToString() + " : " + second.ToString();
+        }
+
+        //Debug.Log("text: "+text.gameObject.name);
+        //Debug.Log();
         if (timer > 0 && isRunning)
         {
             timer -= Time.deltaTime;
