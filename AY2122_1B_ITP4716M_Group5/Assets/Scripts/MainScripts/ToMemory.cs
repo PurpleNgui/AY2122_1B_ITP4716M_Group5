0d8fa4f8de1ebecca30db5e7e847306a704cf1e2
PurpleNgui <210453770@stu.vtc.edu.hk>
2022-04-23 18:58:18 +0800

    switch to memory scene

---
+++ AY2122_1B_ITP4716M_Group5/Assets/Scripts/MainScripts/ToMemory.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToMemory : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.name == "FPSController")
+        {
+            Debug.Log("Touch");
+            SceneManager.LoadScene("MemoryGameScene");
+        }
+    }
+}
---
+++ AY2122_1B_ITP4716M_Group5/Assets/Scripts/MainScripts/ToSkipping.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToSkipping : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.name == "FPSController")
+        {
+            Debug.Log("Touch");
+            SceneManager.LoadScene("MemoryGameScene");
+        }
+    }
+}
