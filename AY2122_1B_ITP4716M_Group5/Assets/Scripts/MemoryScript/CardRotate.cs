f2e5e721b8e54b980bec1dcfc967df1f30eaae14
PurpleNgui <210453770@stu.vtc.edu.hk>
2022-04-24 23:48:42 +0800

    Update CardRotate.cs

--- AY2122_1B_ITP4716M_Group5/Assets/Scripts/MemoryScript/CardRotate.cs
+++ AY2122_1B_ITP4716M_Group5/Assets/Scripts/MemoryScript/CardRotate.cs
@@ -36,6 +36,8 @@ public class CardRotate : MonoBehaviour
         {
             targetRotY += 180;
         }
+
+        //Debug.Log("Select " + name);
     }
 
     //public GameObject getSelectedCard()
