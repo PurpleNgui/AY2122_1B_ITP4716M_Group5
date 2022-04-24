f83f8a324399c5590a8f21b602957eb5bd4f458b
SzeFungTing <210113787@stu.vtc.edu.hk>
2022-04-24 23:38:34 +0800

    b

---
+++ AY2122_1B_ITP4716M_Group5/Assets/Scripts/SkippingScript/UI/RulesScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RulesScript : MonoBehaviour
+{
+    public Button lastPageBtn;
+    public Button nextPageBtn;
+    public Button closeBtn;
+
+    private int currentPage = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
